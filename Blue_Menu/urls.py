dfa21f794cc5db532d5853b9a654e8ce3449b2fa
Brzydal <damianfilipkowski@gmail.com>
2017-08-15 16:11:47 +0200

    Urls for Card created

--- Blue_Menu/urls.py
+++ Blue_Menu/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from menu.views import CardView, CardsView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^cards/$', CardsView.as_view(), name='cards'),
+    url(r'^card/(?P<pk>(\d)+)/$', CardView.as_view(), name='card'),
+
 ]
