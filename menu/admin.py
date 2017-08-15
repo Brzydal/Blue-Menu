a600f992304aeba7b46206d896d97cab811645ea
Brzydal <damianfilipkowski@gmail.com>
2017-08-15 13:55:43 +0200

    Both models registered in admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from menu.models import Card, Meal
 
-# Register your models here.
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    pass
+    # Description of List in Admin
+    # list_display = \
+    #     ['id', 'name', 'description', 'image_tag', 'main_category', 'level', 'lottery', 'date_created', 'date_updated']
+    #
+    # list_filter = ('main_category', 'lottery',)
+    # list_search = ('name',)
+    # list_editable = ('description',)
+    # ordering = ('id', 'level', 'date_created', 'date_updated')
+    # list_display_links = ('name',)
+    #
+    # # Description of Form Edit in Admin
+    # readonly_fields = ['date_created', 'date_updated']
+
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    pass
+    # Description of List in Admin
+    # list_display = ['id', 'name', 'visible']
+    # list_filter = ('visible',)
+    # list_search = ('name',)
+    # ordering = ('id',)
+    # list_display_links = ('name',)
