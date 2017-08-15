d22a5b6c9572371bb731173cf9ceef4534c1c6c8
Brzydal <damianfilipkowski@gmail.com>
2017-08-15 13:49:36 +0200

    model Meal created

--- menu/models.py
+++ menu/models.py
@@ -22,30 +22,30 @@ class Card(models.Model):
         return reverse('card', kwargs={'pk': self.id})
 
 
-# class Meal(models.Model):
-#     """
-#     This model represents Meal.
-#     """
-#
-#     id = models.IntegerField(primary_key=True)
-#     name = models.CharField(max_length=64)
-#     description = models.TextField()
-#     logo = models.CharField(max_length=256, null=True)
-#     main_category = models.ForeignKey(Category)
-#     categories = models.CharField(max_length=64, null=True)
-#     level = models.IntegerField()
-#     lottery = models.BooleanField()
-#     date_created = models.DateTimeField(auto_now_add=True)
-#     date_updated = models.DateTimeField(auto_now=True)
-#
-#     def __str__(self):
-#         return self.name
-#
-#     def get_absolute_url(self):
-#         return reverse('shop', kwargs={'pk': self.id})
-#
-#     def image_tag(self):
-#         return u'<img src="%s" width="50" height="50" />' % self.logo
-#
-#     image_tag.short_description = 'Image'
-#     image_tag.allow_tags = True
+class Meal(models.Model):
+    """
+    This model represents Meal.
+    """
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.DecimalField()
+    preparation_time = models.IntegerField()
+    vegetarian = models.BooleanField()
+    picture = models.ImageField(null=True)
+    main_category = models.ForeignKey(Category)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('meal', kwargs={'pk': self.id})
+
+    # def image_tag(self):
+    #     return u'<img src="%s" width="50" height="50" />' % self.logo
+    #
+    # image_tag.short_description = 'Image'
+    # image_tag.allow_tags = True
