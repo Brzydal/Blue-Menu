d649b9c79bb16955056c41c696b01d3394169063
Brzydal <damianfilipkowski@gmail.com>
2017-08-15 16:21:58 +0200

    templates for card and cards completed

--- menu/views.py
+++ menu/views.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import Card, Meal
 
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
-from django.views.generic.edit import (
-    CreateView,
-    DeleteView, )
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
 
 from .models import Card
-from .serializers import ShopSerializer
 
 
 class CardsView(ListView):
