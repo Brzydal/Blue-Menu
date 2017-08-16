0c58fde22be922ae897b118de079d72f4c5cfd85
Brzydal <damianfilipkowski@gmail.com>
2017-08-16 19:18:58 +0200

    Models updated - created_at updated_at

--- menu/models.py
+++ menu/models.py
@@ -8,15 +8,14 @@ class Meal(models.Model):
     """
     This model represents Meal.
     """
-
     name = models.CharField(max_length=64)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     preparation_time = models.IntegerField()
     vegetarian = models.BooleanField()
     picture = models.ImageField(upload_to='menu/static/menu/pictures', null=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -36,12 +35,11 @@ class Card(models.Model):
     """
     This model represents Menu Card.
     """
-
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField()
     meal = models.ManyToManyField(Meal, blank=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
