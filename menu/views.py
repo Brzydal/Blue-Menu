00c2b47df7c76d275985d9797efafaf016db2672
Brzydal <damianfilipkowski@gmail.com>
2017-08-16 19:30:37 +0200

    utf removed

--- menu/views.py
+++ menu/views.py
@@ -1,15 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
-from django.views.generic.edit import (
-    CreateView,
-    DeleteView, )
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
