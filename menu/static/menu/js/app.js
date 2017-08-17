2af3def37a41a97ff9e569d44d4032129322f00a
Brzydal <damianfilipkowski@gmail.com>
2017-08-18 00:47:23 +0200

    List render added

--- menu/static/menu/js/app.js
+++ menu/static/menu/js/app.js
@@ -12,6 +12,19 @@ client.get("http://127.0.0.1:8000/cardsAPI/").then(function(data) {
     console.log(schema)
 })
 
+//var ProperListRender = React.createClass({displayName: "ProperListRender",
+//render: function() {
+//  return (
+//    React.createElement("ul", null,
+//      this.props.list.map(function(listValue){
+//        return React.createElement("li", null, listValue);
+//      })
+//    )
+//  )
+//}
+//});
+//React.render(React.createElement(ProperListRender, {list: [1,2,3,4,5]}), document.getElementById('proper-list-render1'));
+//React.render(React.createElement(ProperListRender, {list: [1,2,3,4,5,6,7,8,9,10]}), document.getElementById('proper-list-render2'));
 
 
 });
