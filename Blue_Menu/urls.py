3d2c0108e4912618e22415dca4202982804ce4cf
Brzydal <damianfilipkowski@gmail.com>
2017-08-20 21:13:09 +0200

    requirements updated 000 reoved

--- Blue_Menu/urls.py
+++ Blue_Menu/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 from menu.views import CardDetailView, CardListView, FinalView
 from menu.api.views import CardListApiView, CardRetrieveApiView
 from rest_framework.documentation import include_docs_urls
@@ -22,13 +23,14 @@ from rest_framework.documentation import include_docs_urls
 from django.conf import settings
 from django.conf.urls.static import static
 
-if settings.DEBUG:
-    import debug_toolbar
+import debug_toolbar
 
 urlpatterns = [
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^docs/', include_docs_urls(title='My API service')),
+    url(r'^$', RedirectView.as_view(pattern_name='cards', permanent=False)),
 
     url(r'^cards/$', CardListView.as_view(), name='cards'),
     url(r'^card/(?P<pk>(\d)+)/$', CardDetailView.as_view(), name='card'),
@@ -37,6 +39,7 @@ urlpatterns = [
     url(r'^cardsAPI/$', CardListApiView.as_view(), name='cards-api-list'),
     url(r'^cardsAPI/(?P<pk>(\d)+)/$', CardRetrieveApiView.as_view(), name='cards-api-retrieve'),
 
-    # url(r'^__debug__/', include(debug_toolbar.urls)),
+    url(r'^__debug__/', include(debug_toolbar.urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
